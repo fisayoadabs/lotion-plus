c5d0f75ac59c4dc2b8e88970bf76a297f457fcdb
Fizzy001 <oluwafisayo.adabs@ucalgary.ca>
2023-03-23 20:47:44 -0600

    Function Delete And Get

--- functions/delete-note/main.py
+++ functions/delete-note/main.py
@@ -1,1 +1,27 @@
-# add your delete-note function here
+import json
+import boto3
+from boto3.dynamodb.conditions import Key
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("notes-30141921")
+
+def delete_note_handler(event, context):
+    email = event["queryStringParameters"]["email"]
+    id = event["queryStringParameters"]["id"]
+
+    try:
+        table.delete_item(Key={
+            "email": email,
+            "id": id,
+        })
+        res = table.query(KeyConditionExpression=Key("eamil").eq(email))
+        return{
+            "statusCode": 200,
+            "body": "success"
+        }
+    except Exception:
+        print(Exception)
+        return{
+            "statusCode": 500,
+            "body": json.dumps({"message": str(Exception)})
+        }
--- functions/get-notes/main.py
+++ functions/get-notes/main.py
@@ -1,1 +1,22 @@
-# add your get-notes function here
+import json
+import boto3
+from boto3.dynamodb.conditions import Key
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("notes-30141921")
+
+def get_notes_handler(event, context):
+    email = event["queryStringParameters"]["email"]
+
+    try:
+        res = table.query(KeyConditionExpression=Key("eamil").eq(email))
+        return{
+            "statusCode": 200,
+            "body": json.dumps(res["Items"])
+        }
+    except Exception:
+        print(Exception)
+        return{
+            "statusCode": 500,
+            "body": json.dumps(res["Items"])
+        }
